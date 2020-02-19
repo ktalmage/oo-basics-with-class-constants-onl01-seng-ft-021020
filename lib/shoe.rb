1bd34e456077bbad86bc5066a8f21fc089c56082
Kyle Talmage <ktalmage85@gmail.com>
2020-02-19 15:17:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    if !@brand.include? (brand)
-    BRANDS << @brand
+    if !BRANDS.include? (brand)
+    BRANDS << brand
     end
    end
 
